8bed985f5597b6bbf4de48fc7152b69098e8fb13
jinjianli <jinjianli21@163.com>
2020-12-05 18:08:51 +0900

    second

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -22,6 +22,8 @@ class ComposerAutoloaderInitae09fc6f9eeed9c14c95d01d7abdaa40
             return self::$loader;
         }
 
+        require __DIR__ . '/platform_check.php';
+
         spl_autoload_register(array('ComposerAutoloaderInitae09fc6f9eeed9c14c95d01d7abdaa40', 'loadClassLoader'), true, true);
         self::$loader = $loader = new \Composer\Autoload\ClassLoader();
         spl_autoload_unregister(array('ComposerAutoloaderInitae09fc6f9eeed9c14c95d01d7abdaa40', 'loadClassLoader'));
@@ -32,7 +34,7 @@ class ComposerAutoloaderInitae09fc6f9eeed9c14c95d01d7abdaa40
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
         if ($useStaticLoader) {
-            require_once __DIR__ . '/autoload_static.php';
+            require __DIR__ . '/autoload_static.php';
 
             call_user_func(\Composer\Autoload\ComposerStaticInitae09fc6f9eeed9c14c95d01d7abdaa40::getInitializer($loader));
         } else {
